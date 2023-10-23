2436db2f3f6485ef9667b958a034a4c55b213983
DomineersXII <channen1titan@gmail.com>
2023-10-23 17:31:37 -0400

    refactoring

--- index.js
+++ index.js
@@ -1,46 +1,192 @@
+class GameObject {
+    constructor(
+        game,
+        height, 
+        width, 
+        id, 
+        speed, 
+        defaultX, 
+        defaultyY,
+      
+    ) {
+        this.game = game
+        this.height = height
+        this.width = width
+        this.element = document.getElementById(id)
+        this.speed = speed
+        this.defaultX = defaultX ?? game.width /2 - width/2
+        this.defaultY = this.defaultY ?? game.height /2 - height/2
+        this.position = {
+            x:0,
+            y:0,
+        }
+        this.velocity = {
+            x:0,
+            y:0,
+        }
+    }
+    resetPosition() {
+        this.position.x = this.defaultX
+        this.position.y = this.defaultY
+    }
+    render() {
+        this.element.style.left = this.position.x + "px"
+        this.element.style.top = this.position.y + "px"
+
+    }
+    move() {
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+    }
+}
+
+class Player extends GameObject {
+constructor(
+    game,
+    height, 
+    width, 
+    id, 
+    speed, 
+    defaultX, 
+    defaultY,
+  
+) {
+    super(game, height,width,id,speed,defaultX,defaultY)
+    this.isUp = false
+    this.isDown = false
+    this.score = 0
+}
+moveUp() {
+    if(!this.game.running) return
+    if(this.position.y <= 0) return
+    this.position.y -= this.speed
+}
+moveDown() {
+    if(!this.game.running) return
+    if(this.position.y >= this.game.height - this.height) return
+    this.position.y += this.speed
+}
+update() {
+    if(this.isUp) {
+        this.moveUp()
+
+    }
+    if(this.isDown) {
+        this.moveDown()
+    }
+    this.render()
+}
+}
+
+class Ball extends GameObject {
+    constructor(game, height,width,id,speed,defaultX,defaultY) {
+        super(game, height,width,id,speed,defaultX,defaultY)
+    }
+         resetPosition() {
+         super.resetPosition()
+              if (Math.random() < 0.5) {
+                  this.velocity.x = this.speed
+               } else {
+                  this.velocity.x = -this.speed   
+               }
+               
+            this.velocity.y = this.speed*Math.sin(Math.floor(Math.random() * (Math.PI/3 - -Math.PI/3) ) -Math.PI/3)
+            console.log(this.velocity.y)
+        }
+
+        update() {
+            this.move()
+            this.checkWall()
+            this.checkPlayerCollision(this.game.leftPlayer)
+            this.checkPlayerCollision(this.game.rightPlayer)
+            this.render()
+        }
+        checkWall() {
+
+            if(this.position.y + this.height > this.game.height) {
+                this.velocity.y = -this.speed
+            }
+            if(this.position.y < 0) {
+                this.velocity.y = this.speed
+            }
+            if(this.position.x < 0) {
+              this.game.rightPlayer.score++
+                scoreBoard.update()
+                this.resetPosition()
+            }
+            if(this.position.x + this.width > this.game.width) {
+                this.game.leftPlayer.score++
+                scoreBoard.update()
+                this.resetPosition()
+            }
+        }
+        checkPlayerCollision(player) {
+            if(this.position.x < player.position.x + player.width 
+                && this.position.x + this.width > player.position.x 
+                && this.position.y > player.position.y 
+                && this.position.y < player.position.y + player.height
+                ) {
+                     
+                    this.velocity.x *= -1
+                    this.velocity.y = this.speed * Math.sin(-Math.PI/3 *
+                         ((player.position.y + player.height/2 - this.position.y)/
+                         (player.height/2)))
+            }
+        }
+    
+}
+
 const game = {
     height: 500,
     width: 500,
     running: false,
+    leftPlayer: null,
+    rightPlayer: null,
+    ball: null,
+
     init() {
-        leftPlayer.resetPosition()
-        rightPlayer.resetPosition()
-        ball.resetPosition()
+        this.leftPlayer = new Player (this, 50, 10, "leftPaddle", 3.5, 0, null)
+        this.rightPlayer = new Player (this, 50, 10, "rightPaddle", 3.5, 490, null)
+        this.ball = new Ball(this,10,10, "ball",1.2,null,null)
+     
+        this.leftPlayer.resetPosition()
+        this.rightPlayer.resetPosition()
+        this.ball.resetPosition()
         document.addEventListener("keydown", (event) => {
             if(event.key ==="s") {
-                leftPlayer.isDown = true
+             this.leftPlayer.isDown = true
             }
             if(event.key ==="w") {
-                leftPlayer.isUp = true
+                this.leftPlayer.isUp = true
             }
             if(event.key ==="ArrowDown") {
-                rightPlayer.isDown = true
+                this.rightPlayer.isDown = true
             }
             if(event.key ==="ArrowUp") {
-                rightPlayer.isUp = true
+                this.rightPlayer.isUp = true
             }
         })
 
         document.addEventListener("keyup", (event) => {
             if(event.key ==="s") {
-                leftPlayer.isDown = false
-                leftPlayer.moveDown()
+                this.leftPlayer.isDown = false
+                this.leftPlayer.moveDown()
             }
             if(event.key ==="w") {
-                leftPlayer.isUp = false
+                this.leftPlayer.isUp = false
             }
             if(event.key ==="ArrowDown") {
-                rightPlayer.isDown = false
+                this.rightPlayer.isDown = false
             }
             if(event.key ==="ArrowUp") {
-                rightPlayer.isUp = false
+                this.rightPlayer.isUp = false
             }
         })
 
         this.running = false
-        leftPlayer.update()
-        rightPlayer.update()
-        ball.update()
+        this.leftPlayer.update()
+        this.rightPlayer.update()
+        this.ball.update()
         menu.init()
     },
     animate() {
@@ -50,97 +196,15 @@ const game = {
     },
     update() {
         if(this.running) {
-            leftPlayer.update()
-            rightPlayer.update()
-            ball.update()
+            this.leftPlayer.update()
+            this.rightPlayer.update()
+            this.ball.update()
         }
        
     },
 }
 
-const leftPlayer = {
-    element: document.getElementById("leftPaddle"),
-    height: 50,
-    width: 10,
-    speed: 3.5,
-    position: {
-        x: 0,
-        y: 0,   
-    },
-    score: 0,
-    isUp: false,
-    isDown: false,
-    resetPosition() {
-        this.position.x = 0
-        this.position.y = game.height/2 - this.height/2
-    },
-    render() {
-        this.element.style.left = this.position.x + "px"
-        this.element.style.top = this.position.y + "px"
-    },
-    moveUp() {
-        if(!game.running) return
-        if(this.position.y <= 0) return
-        this.position.y -= this.speed
-    },
-    moveDown() {
-        if(!game.running) return
-        if(this.position.y >= game.height - this.height) return
-        this.position.y += this.speed
-    },
-    update() {
-        if(this.isUp) {
-            this.moveUp()
 
-        }
-        if(this.isDown) {
-            this.moveDown()
-        }
-        this.render()
-    },
-}
-
-
-const rightPlayer = {
-    element: document.getElementById("rightPaddle"),
-    height: 50,
-    width: 10,
-    speed: 3.5,
-    position: {
-        x: 0,
-        y: 0,   
-    },
-    score: 0,
-    resetPosition() {
-        this.position.x = 490
-        this.position.y = game.height/2 - this.height/2
-      
-    },
-    render() {
-        this.element.style.left = this.position.x + "px"
-        this.element.style.top = this.position.y + "px"
-    },
-    moveUp() {
-        if(!game.running) return
-        if(this.position.y <= 0) return
-        this.position.y -= this.speed
-    },
-    moveDown() {
-        if(!game.running) return
-        if(this.position.y >= game.height - this.height) return
-        this.position.y += this.speed
-    },
-    update() {
-        if(this.isUp) {
-            this.moveUp()
-
-        }
-        if(this.isDown) {
-            this.moveDown()
-        }
-        this.render()
-    },
-}
 
 const ball = {
     element: document.getElementById("ball"),
@@ -221,7 +285,7 @@ const scoreBoard = {
     element: document.getElementById("score"),
     text: "0-0",
     update() {
-        this.text = `${leftPlayer.score} - ${rightPlayer.score}`
+        this.text = `${game.leftPlayer.score} - ${game.rightPlayer.score}`
         this.element.innerText = this.text
     },
 
@@ -239,4 +303,6 @@ init() {
     })
 },
 }
+
+
 game.init()
